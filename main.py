4afcc228f353d8216878d80517f68202e84fa909
L1STOV <listovspartak99@gmail.com>
2021-01-30 18:35:03 +0200

    task class created

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import time
 from plyer import notification
 
+
+class Pomodoro:
+    def __init__(self, task_name, pomodoro_count=1, pomodoro_duration=25, breaks=0, break_duration=5):
+        self.task_name = task_name
+        self.pomodoro_count = pomodoro_count
+        self.pomodoro_duration = pomodoro_duration
+        self.breaks = breaks
+        self.break_duration = break_duration
+
+    def notifier(self):
+        pass
+
+
 if __name__ == '__main__':
-    while True:
-        notification.notify(
-            title='Break needed',
-            message='Please, take a break',
-            app_icon='img/tomato_1.ico',
-            timeout=10
-        )
-        time.sleep(60 * 25)
+    pass
