df718acd6b375d92eb2469e0ccf3310c729f5270
L1STOV <listovspartak99@gmail.com>
2021-01-31 13:47:12 +0200

    added label + ico

--- gui.py
+++ gui.py
@@ -1,15 +1,20 @@
 from tkinter import *
 
+root = Tk()
+
 
 def gui():
-    root = Tk()
 
     root['bg'] = 'red'
     root.title('Pomodoro')
-    root.wm_attributes('-alpha', 0.7)
+    root.iconbitmap('img/tomato_1.ico')
+    root.wm_attributes('-alpha', 0.9)
     root.geometry('500x250')
-
     root.resizable(width=False, height=False)
+
+    pomodoro_label = Label(root, text='Pomodoro timer', font=('Comic Sans MS', 25, 'bold'))
+    pomodoro_label.pack()
+    pomodoro_label.config(bd=20, bg='red')
 
     root.mainloop()
 
